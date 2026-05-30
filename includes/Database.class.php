d36e77668b89da46d6c332bd624d719d58fde0f8
Luka <khundadzeluka792@gmail.com>
2026-05-30 20:00:04 +0400

    PDO connecting to wrong db path fix

--- includes/Database.class.php
+++ includes/Database.class.php
@@ -6,7 +6,7 @@ class Database
 
     private function __construct()
     {
-        $this->pdo = new PDO('sqlite:' . __DIR__ . DB_PATH);
+        $this->pdo = new PDO('sqlite:' . DB_PATH);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     }
