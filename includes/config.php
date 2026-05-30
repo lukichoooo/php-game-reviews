783bb10bf6ec9e4bf0f3c947a2c7e5a1cf7ddf06
Luka <khundadzeluka792@gmail.com>
2026-05-30 19:54:17 +0400

    fix config.php

--- includes/config.php
+++ includes/config.php
@@ -2,4 +2,9 @@
 session_start();
 
 define('DB_PATH', __DIR__ . '/../gamereviews.db');
-define('UPLOAD_DIR', __DIR__ . '/../gamereviews.db');
+define('UPLOAD_DIR', __DIR__ . '/../uploads/');
+
+require_once __DIR__ . '/dtos/ResponseDto.class.php';
+require_once __DIR__ . '/Database.class.php';
+require_once __DIR__ . '/User.class.php';
+require_once __DIR__ . '/Review.class.php';
